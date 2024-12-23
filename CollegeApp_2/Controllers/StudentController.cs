6d247fb3a92e1ced7225d6401d9b905a04f6ad04
K-yun <K-yun@YUNUS>
2024-12-23 21:03:45 +0300

    Proje dosyası ekle.

---
+++ CollegeApp_2/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudentName()
+        {
+            return "Student Name 1";
+        }
+    }
+}
