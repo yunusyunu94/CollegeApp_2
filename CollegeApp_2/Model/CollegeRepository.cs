286902bd1c2e78a0eeb2eb0fcb00a390f0402a5a
Yunus Kartal <K-yunus@hotmail.com>
2024-12-25 22:37:37 +0300

    2_Modelde_CollegeRepositoryde_elle_girilen_verileri_tasıdık

---
+++ CollegeApp_2/Model/CollegeRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace CollegeApp_2.Model
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>(){
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Student 1",
+                    Email ="Studentemail1@hotmail.com",
+                    Adres = "Hyd, INDIA"
+
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Student 2",
+                    Email = "Studentemail1@hotmai2.com",
+                    Adres = "Banglore, INDIA"
+
+                }
+         };
+    }
+}
