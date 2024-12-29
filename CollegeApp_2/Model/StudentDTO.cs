d8ef3e89ab3f99985e13e36734458db1e66b434d
Yunus Kartal <K-yunus@hotmail.com>
2024-12-29 16:40:06 +0300

    8_Validation_StudentDTO

--- CollegeApp_2/Model/StudentDTO.cs
+++ CollegeApp_2/Model/StudentDTO.cs
@@ -1,10 +1,18 @@
-﻿namespace CollegeApp_2.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeApp_2.Model
 {
     public class StudentDTO
     {
         public int Id { get; set; }
+
+        [Required]
         public string StudentName { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Adres { get; set; }
     }
 }
