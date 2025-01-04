20c9a2d96c63b91f48bedab2edb3c69c8ea5d6b9
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-04 11:53:17 +0300

    14_Dependency_Injection_ProgramCsde_Containerolusturma_2

--- CollegeApp_2/Program.cs
+++ CollegeApp_2/Program.cs
@@ -1,3 +1,4 @@
+using CollegeApp_2.Mylogging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
@@ -13,6 +14,17 @@ builder.Services.AddControllers(Options => Options.ReturnHttpNotAcceptable = tru
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/// ---------------------- 2. Loosely coumpled ------------------------
+
+builder.Services.AddScoped<IMyLogger, LogToFile>();
+
+
+
+
+
+
 
 var app = builder.Build();
 
