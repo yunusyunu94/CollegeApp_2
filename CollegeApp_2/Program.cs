372e921e376b38b3e8d08030dd02a9b4b71076f5
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-04 16:54:55 +0300

    15_Logger_StudentController_Ve_ProgramCs_Hata_Gunluk_Kayitlari_Konsola_Yazdirma

--- CollegeApp_2/Program.cs
+++ CollegeApp_2/Program.cs
@@ -1,10 +1,25 @@
+using System;
 using CollegeApp_2.Mylogging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Logger ;
 var builder = WebApplication.CreateBuilder(args);
+// bir kayit chazini kullanmak ;
+// Once konsoru temizlicez ;
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole(); // Yalnizca konsol icin
+//builder.Logging.AddDebug(); // Yalnizca hata ayiklama giris yapmak icin
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 // Add services to the container.
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /// Postmanda Json istek dýsýnda faklý format istek alinirsa hada mesaji dondermemiz gerekir bunun icin AddControllers() icine " Options => Options.ReturnHttpNotAcceptable = true " yazmaliyiz ;
 /// Json formati icin   ; AddNewtonsoftJson()
@@ -15,12 +30,38 @@ builder.Services.AddControllers(Options => Options.ReturnHttpNotAcceptable = tru
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /// ---------------------- 2. Loosely coumpled ------------------------
 
-builder.Services.AddScoped<IMyLogger, LogToFile>();     // Ýstege ( reguest ) 1 neste uretir 
+// DemoController devami
+
+// builder.Services.AddScoped<IMyLogger, LogToFile>();     // Ýstege ( reguest ) 1 neste uretir 
 builder.Services.AddSingleton<IMyLogger, LogToFile>();  // Her ayri istege ( reguest ) neste uretir 
-builder.Services.AddTransient<IMyLogger, LogToFile>();  // Her Ýstege ( reguest ) ayri ayri neste uretir 
+//builder.Services.AddTransient<IMyLogger, LogToFile>();  // Her Ýstege ( reguest ) ayri ayri neste uretir 
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// ----------------------------------------- Logger ------------------------------------------------------------------
+
+// Console  ( Konsol Gunluk Kaydi  ) ;
+// StudentController de loglamayi yaptik konsol ekraninda görabiliriz
+
+
+// Debug    ( Hata ayýklama Gunluk Kaydi  );
+// Sadece bir kayit chazini kullanmak istiyorsak yukaridaki  " var builder = WebApplication.CreateBuilder(args); " kisnida adimlari izlicez
+
+
+// EventSource  ( OlayKaynaðý Gunluk Kaydi  )
+// EventLog (EventLog (yalnýzca Windows) Gunluk Kaydi )
+
+// Yukaridakileri dogrudan kullanmamizi saglayan en uste    " var builder = WebApplication.CreateBuilder(args); " yapisi sayesinde Dependency Injection icin dogrudan kullanilabilir olacaktir
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 
