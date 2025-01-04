02b47b803f7a830680870a43b43c90df1ad177c6
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-04 09:35:24 +0300

    14_Dependency_Injection_Model_Klosoru_Olusrurduk_Ve_DemoController

---
+++ CollegeApp_2/Mylogging/LogToFile.cs
@@ -0,0 +1,13 @@
+﻿namespace CollegeApp_2.Mylogging
+{
+    public class LogToFile : IMyLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("LogToFile");
+
+            // write your own logic to save the logs to file
+        }
+    }
+}
---
+++ CollegeApp_2/Mylogging/LogToServerMemory.cs
@@ -0,0 +1,16 @@
+﻿namespace CollegeApp_2.Mylogging
+{
+    public class LogToServerMemory : IMyLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("LogToServerManager");
+
+            // write your own logic to save the logs to Server memory
+
+        }
+    
+       
+    }
+}
