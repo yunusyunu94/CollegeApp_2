82ab6329bd6db03d9b625fa44b474993dbb41ccb
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-17 21:26:07 +0300

    16_DataKlasoruOlusturduk_İcerisineStudentTablosuOlusturduk_Ve_VeriTabaniBaglaantisi_CollegeDBContext_Olusturduk

---
+++ CollegeApp_2/Data/CollegeDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CollegeApp_2.Data
+{
+    public class CollegeDBContext : DbContext
+    {
+        DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CollegeApp_2/Data/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CollegeApp_2.Data
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string Adres { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
