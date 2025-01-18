5572c2d38d36fb4acfa051b3983d9b17f2e95e1c
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-18 20:16:43 +0300

    16_Dataİcerisine_ConfigKlasoruOlusturduk_Ve_StudentConfikSinifiOLusturup_ConfigurasyonVeVeriİslemleriniBirSinifaTopladik

--- CollegeApp_2/Data/CollegeDBContext.cs
+++ CollegeApp_2/Data/CollegeDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CollegeApp_2.Data.Config;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollegeApp_2.Data
 {
@@ -14,41 +15,11 @@ namespace CollegeApp_2.Data
        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Vari tabanina elle veri ekleme;
-
-            modelBuilder.Entity<Student>().HasData(new List<Student>
-            {
-                new Student
-                {
-                    Id = 1,
-                    StudentName = "Yunus",
-                    Email ="yunus@hotmail.com",
-                    Adres = "Hyd, INDIA",
-                    DOB = new DateTime(2022,12,12)
-                     
-
-                },
-                new Student
-                {
-                    Id = 2,
-                    StudentName = "Anil",
-                    Email = "Anil@hotmail.com",
-                    Adres = "Banglore, INDIA",
-                    DOB = new DateTime(2022,06,12)
-
-                }
-            });
+            // Tablo 1
+            modelBuilder.ApplyConfiguration(new StudentConfig()); // StudentConfig sinifini burda tanimliyoruz.Her tablo icin ayri ayri tanimliyoruz
 
 
-            // Student sinifini ozellestirelim ;
-
-            modelBuilder.Entity<Student>(entity =>
-            {
-                entity.Property(n => n.StudentName).IsRequired();   // StudentName gereklidir.
-                entity.Property(n =>n.Email).HasMaxLength(250);     // Maxsimim 250 karakter.
-                entity.Property(n => n.Adres).IsRequired(false).HasMaxLength(500);    // Adres istege bagli.Maksimum 500 karakter.
-                entity.Property(n => n.Email).IsRequired().HasMaxLength(250);         // Email gereklidir.Maksimum 250 karakter.
-            });
+           
         }
     }
 }
