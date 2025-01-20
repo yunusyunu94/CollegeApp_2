4d9f31c6da0b36e4c46fd88d8aa3236e9a3dc14d
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-20 23:09:41 +0300

    18_AUTOMAPPER--ConfigurationAdliKlasorOlusturdukİcerisine_AutoMapperConfigAdindaSinifOlusturduk

---
+++ CollegeApp_2/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CollegeApp_2.Data;
+using CollegeApp_2.Model;
+
+namespace CollegeApp_2.Configurations
+{
+
+    // Nugetten yukleyecegimiz kutuphaneler ; " AutoMapper "  VE  " AutoMapper.Extensions.Microsoft.DependencyInjection "
+    // AutoMapper icin " Profile " den miras almaliyiz
+    public class AutoMapperConfig : Profile
+    {
+         public AutoMapperConfig()
+        {
+            //CreateMap<Student, StudentDTO>(); // Student verileri StudentDTO ya kopyaliyoruz
+            //CreateMap<StudentDTO, Student>(); // StudentDTO verileri Student de kopyaliyoruz
+
+            // VEA
+
+            CreateMap<Student, StudentDTO>().ReverseMap();  // Yukaridaki 2 kodla ayni kisaltmasi
+
+            // Daha sonra Program.cs 
+        }
+    }
+}
