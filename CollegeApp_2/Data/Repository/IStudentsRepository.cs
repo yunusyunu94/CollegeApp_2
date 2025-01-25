9d78e3d99352fec7bb3f85d1905b6815d7f41694
K-yun <K-yun@DESKTOP-6E087Q9>
2025-01-25 12:53:21 +0300

    19_Datade_RepositoryKlasoruOlusturduk_İcerisine_IStudentRepository_Ve_StudentRepository_olusturduk

---
+++ CollegeApp_2/Data/Repository/IStudentsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace CollegeApp_2.Data.Repository
+{
+    public interface IStudentsRepository
+    {
+        Task <List<Student>> GetAllAsync();
+        Task<Student> GetByIdAsync(int id);
+         Task <Student> GetByNameAsync(string name);
+         Task <int> CreateAsync(Student student);
+        Task<int> UpdateAsync(Student student);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ CollegeApp_2/Data/Repository/StudentsRepository.cs
@@ -0,0 +1,76 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeApp_2.Data.Repository
+{
+    public class StudentsRepository : IStudentsRepository
+    {
+        private readonly CollegeDBContext _dbContext;
+        public StudentsRepository(CollegeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            return await _dbContext.Students.ToListAsync();
+        }
+
+
+        public async Task<Student> GetByIdAsync(int id)
+        {
+            return await _dbContext.Students.Where(Student => Student.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Student> GetByNameAsync(string name)
+        {
+            return await _dbContext.Students.Where(Student => Student.StudentName.ToLower().Equals(name.ToLower())).FirstOrDefaultAsync();
+            // Buyuktur/Kucuktur harflere duyarli olsun
+        }
+
+
+        public async Task<int> CreateAsync(Student student)
+        {
+            _dbContext.Students.Add(student);
+
+            await _dbContext.SaveChangesAsync();
+
+            return student.Id;
+        }
+
+
+        public async Task <int> UpdateAsync(Student student)
+        {
+            var studentToUpdate = await _dbContext.Students.Where(student => student.Id == student.Id).FirstOrDefaultAsync();
+
+            if (studentToUpdate == null)
+                throw new ArgumentException($"No student fount with id: {student.Id}");
+
+            studentToUpdate.StudentName = student.StudentName;
+            studentToUpdate.Email = student.Email;
+            studentToUpdate.Adres = student.Adres;
+            studentToUpdate.DOB = student.DOB;
+
+            await _dbContext.SaveChangesAsync();
+            return student.Id;
+        
+        }
+
+        public async Task <bool> DeleteAsync(int id)
+        {
+            var studentToDelete = await _dbContext.Students.Where(student => student.Id == student.Id).FirstOrDefaultAsync();
+
+            if (studentToDelete == null)
+                throw new ArgumentException($"No student fount with id: {id}");
+
+            _dbContext.Students.Remove(studentToDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+
+        }
+
+
+        
+    }
+}
