4899fb379e17ce7a31f601737a864d453e03ddfa
K-yun <K-yun@DESKTOP-6E087Q9>
2025-02-18 21:17:28 +0300

    21_ProgramCS/Controller--Enabling_CORS_Default_Ve_Birden_Fazla_Policy_Ekleme

---
+++ CollegeApp_2/Controllers/MicrosoftController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors(policyName: "AllowOnlyMicrosoft")]  // Program.cs de tanimladigimiz CORS yontemlerinden hangisini kullanicaksak controllere bu sekilde uyguluyoruz
+    public class MicrosoftController : ControllerBase
+    {
+    }
+}
